a1c3ee0390367b235cf118b48af0fcf8c5d6932d
haidar145kahs <haidar.kahs145@gmail.com>
2024-11-06 15:35:49 +0300

    move addToCart into cart.js

--- data/cart.js
+++ data/cart.js
@@ -1,1 +1,19 @@
 export let cart = [];
+
+export function addToCart(productId){
+    let MatchingItem ;
+    cart.forEach((cartItem) => {
+        if(productId === cartItem.productId){
+            MatchingItem = cartItem;
+        };
+    });
+        if(MatchingItem){
+            MatchingItem.quantity+= 1;
+        }
+        else{
+            cart.push({
+                productId: productId ,
+                quantity: 1
+            })
+        }
+};
