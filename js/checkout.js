13a88e5f1d24e28933c56bc70a09960c516e663b
haidar145kahs <haidar.kahs145@gmail.com>
2024-11-20 14:25:37 +0300

    create oop version of cart

--- js/checkout.js
+++ js/checkout.js
@@ -1,4 +1,5 @@
 import { renderOrderSummury } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
+import '../data/cart-oop.js';
 renderOrderSummury();
 renderPaymentSummary();
